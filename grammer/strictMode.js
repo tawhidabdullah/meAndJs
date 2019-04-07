7c5a7676197869dfffe878f5d12d118060120803
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-04-08 00:50:36 +0600

    strict mode

---
+++ grammer/strictMode.js
@@ -0,0 +1,10 @@
+"use strict";
+function cannotUseGlobalVariableInFunction(){
+    x = 'x'; 
+    console.log(x); 
+}; 
+
+cannotUseGlobalVariableInFunction(); 
+
+
+console.log(0343); // octal literals are not allowed in strict mode 
