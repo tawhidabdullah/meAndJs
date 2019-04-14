0d6a4ecbce85a28ba3ddf25434181c708b7b0f69
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-04-14 18:48:25 +0600

    scope..

--- scope/hoisting.js
+++ scope/hoisting.js
@@ -1,3 +1,4 @@
 // The key node is :==>>
 
 // hoisting is the very cool feature of javascript ! and with out hoisting we won't be able to use mutual recursion
+// let and const is also hoised but they have temporal dead zone
