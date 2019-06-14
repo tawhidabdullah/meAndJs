fcf3d500b01833a3c9a35969cb4a90ae08c56f74
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-06-14 22:16:17 +0600

    ..

--- scope/hoisting.js
+++ scope/hoisting.js
@@ -2,4 +2,4 @@
 
 // hoisting is the very cool feature of javascript ! and with out hoisting we won't be able to use mutual recursion
 // let and const is also hoised but they have temporal dead zone
-// hosing is amazing
+// hosing is amazing and awesome 
